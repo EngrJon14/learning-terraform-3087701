500ccf601c4b9e6097b53c60808e6885b6bb6c00
EngrJon14 <168203050+EngrJon14@users.noreply.github.com>
2024-06-23 00:21:26 +0800

    16

--- variables.tf
+++ variables.tf
@@ -2,8 +2,3 @@ variable "instance_type" {
   description = "Type of EC2 instance to provision"
   default     = "t3.nano"
 }
-
-variable "launch_template_id" {
-  description = "The ID of the launch template"
-  type        = string
-}
