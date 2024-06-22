53078e00c6d5ee8fb2ba9a47d2d2f35157b93e66
EngrJon14 <168203050+EngrJon14@users.noreply.github.com>
2024-06-22 22:14:37 +0800

    array for alb

--- main.tf
+++ main.tf
@@ -66,24 +66,24 @@ resource "aws_instance" "blog" {
 }
 
 module "alb" {
-  source = "terraform-aws-modules/alb/aws"
+  source  = "terraform-aws-modules/alb/aws"
   version = "6.0"
 
-  name = "blog-alb"
-  vpc_id = "module.blog_vpc.vpc_id"
-  subnets = module.blog_vpc.public_subnets
-  security_groups = modules.blog_sg.security_group_id
+  name            = "blog-alb"
+  vpc_id          = module.blog_vpc.vpc_id
+  subnets         = module.blog_vpc.public_subnets
+  security_groups = [modules.blog_sg.security_group_id]
 
   target_groups = [
     {
-      name_prefix = "blog-"
+      name_prefix      = "blog-"
       backend_protocol = "HTTP"
-      backend_port = 80
-      target_type = "instance"
+      backend_port     = 80
+      target_type      = "instance"
       targets = {
         my_target = {
           target_id = aws_instance.blog.id
-          port = 80
+          port      = 80
         }
       }
     }
@@ -91,8 +91,8 @@ module "alb" {
 
   http_tcp_listeners = [
     {
-      port = 80
-      protocol = "HTTP"
+      port               = 80
+      protocol           = "HTTP"
       target_group_index = 0
     }
   ]
