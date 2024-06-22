bda34a391e5f2ee07425b720f373065c073ee688
EngrJon14 <168203050+EngrJon14@users.noreply.github.com>
2024-06-22 22:52:42 +0800

    autoscaling

--- main.tf
+++ main.tf
@@ -53,29 +53,33 @@ resource "aws_security_group" "blog" {
   vpc_id = data.aws_vpc.default.id
 }
 
-resource "aws_instance" "blog" {
-  ami                         = data.aws_ami.app_ami.id
-  instance_type               = var.instance_type
-  associate_public_ip_address = true
-  
-  vpc_security_group_ids = [module.blog_sg.security_group_id]
+module "autoscaling_example_complete" {
+  source  = "terraform-aws-modules/autoscaling/aws//examples/complete"
+  version = "7.6.1"
 
-  subnet_id = module.blog_vpc.public_subnets[0]
+  name = "blog"
+  min_size = 1
+  max_size = 2 
 
-  tags = {
-    Name = "Learning Terraform"
-  }
-}
-
-module "alb" {
-  source  = "terraform-aws-modules/alb/aws"
-  version = "6.0"
-
-  name            = "blog-alb"
-  vpc_id          = module.blog_vpc.vpc_id
-  subnets         = module.blog_vpc.public_subnets
+  vpc_zone_identifier = module.blog_vpc.public_subnets
+  target_group_arns = module.blog_alb.target_group_arns
   security_groups = [module.blog_sg.security_group_id]
 
+}
+
+module "blog_alb" {
+  source  = "terraform-aws-modules/alb/aws"
+  version = "6.0"
+  name    = "blog-alb"
+
+  vpc_id                      = module.blog_vpc.vpc_id
+  subnets                     = module.blog_vpc.public_subnets
+  security_groups             = [module.blog_sg.security_group_id]
+  associate_public_ip_address = true
+  
+  image_id         = data.aws_ami.app_ami.id
+  instance_type    = var.instance_type
+  
   target_groups = [
     {
       name_prefix      = "blog-"
@@ -103,5 +107,6 @@ module "alb" {
     Environment = "Dev"
   }
 }
+
 
   
