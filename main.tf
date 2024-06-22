81201fbd327ae9f7438054c8138827c38c612045
EngrJon14 <168203050+EngrJon14@users.noreply.github.com>
2024-06-23 01:13:15 +0800

    21

--- main.tf
+++ main.tf
@@ -1,3 +1,4 @@
+# Define the AWS AMI data source
 data "aws_ami" "app_ami" {
   most_recent = true
 
@@ -14,18 +15,21 @@ data "aws_ami" "app_ami" {
   owners = ["979382823631"] # Bitnami
 }
 
+# Define the default AWS VPC data source
 data "aws_vpc" "default" {
   default = true
 }
 
+# Set up the VPC using the terraform-aws-modules/vpc/aws module
 module "blog_vpc" {
   source = "terraform-aws-modules/vpc/aws"
+  version = "5.8.1"
 
   name = "dev-vpc"
   cidr = "10.0.0.0/16"
 
-  azs             = ["us-west-2a", "us-west-2b", "us-west-2c"]
-  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+  azs            = ["us-west-2a", "us-west-2b", "us-west-2c"]
+  public_subnets = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 
   tags = {
     Terraform   = "true"
@@ -33,19 +37,22 @@ module "blog_vpc" {
   }
 }
 
+# Set up the security groups using the terraform-aws-modules/security-group/aws module
 module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "5.1.2"
-  name    = "blog_new"
 
-  vpc_id               = module.blog_vpc.vpc_id
-  ingress_rules        = ["http-80-tcp", "https-443-tcp"]
-  ingress_cidr_blocks  = ["0.0.0.0/0"]
+  name   = "blog_new"
+  vpc_id = module.blog_vpc.vpc_id
 
-  egress_rules         = ["all-all"]
-  egress_cidr_blocks   = ["0.0.0.0/0"]
+  ingress_rules       = ["http-80-tcp", "https-443-tcp"]
+  ingress_cidr_blocks = ["0.0.0.0/0"]
+
+  egress_rules       = ["all-all"]
+  egress_cidr_blocks = ["0.0.0.0/0"]
 }
 
+# Define a basic AWS security group
 resource "aws_security_group" "blog" {
   name        = "blog"
   description = "Allow http and https in. Allow everything out"
@@ -53,6 +60,7 @@ resource "aws_security_group" "blog" {
   vpc_id = data.aws_vpc.default.id
 }
 
+# Set up the ALB using the terraform-aws-modules/alb/aws module
 module "blog_alb" {
   source  = "terraform-aws-modules/alb/aws"
   version = "6.0"
@@ -61,6 +69,15 @@ module "blog_alb" {
   vpc_id          = module.blog_vpc.vpc_id
   subnets         = module.blog_vpc.public_subnets
   security_groups = [module.blog_sg.security_group_id]
+
+  target_groups = [
+    {
+      name_prefix      = "blog-"
+      backend_protocol = "HTTP"
+      backend_port     = 80
+      target_type      = "instance"
+    }
+  ]
 
   http_tcp_listeners = [
     {
@@ -73,12 +90,18 @@ module "blog_alb" {
   tags = {
     Environment = "Dev"
   }
+
+  // Export the target group ARNs for use in other modules
+  output "target_group_arns" {
+    value = [for tg in aws_alb_target_group.main : tg.arn]
+  }
 }
 
+# Define the launch template for the Auto Scaling Group
 resource "aws_launch_template" "blog_template" {
   name          = "blog_template-launch-template"
   image_id      = data.aws_ami.app_ami.id
-  instance_type = "t2.micro"  # Specify your desired instance type
+  instance_type = "t2.micro"  # Specify your desired instance type here
 
   network_interfaces {
     associate_public_ip_address = true
@@ -90,6 +113,7 @@ resource "aws_launch_template" "blog_template" {
   }
 }
 
+# Set up the Auto Scaling Group using the terraform-aws-modules/autoscaling/aws module
 module "autoscaling" {
   source  = "terraform-aws-modules/autoscaling/aws"
   version = "7.6.1"
@@ -103,5 +127,5 @@ module "autoscaling" {
   desired_capacity = 2
 
   vpc_zone_identifier = module.blog_vpc.public_subnets
-  target_group_arns   = module.blog_alb.target_group_arns
-  }
+  target_group_arns   = module.blog_alb.target_group_arns  // Use target_group_arns from blog_alb module
+}
