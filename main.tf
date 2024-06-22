966ac34dce8b670783b20d74068b7b2efef16337
EngrJon14 <168203050+EngrJon14@users.noreply.github.com>
2024-06-22 23:01:47 +0800

    test

--- main.tf
+++ main.tf
@@ -57,13 +57,13 @@ module "autoscaling_example_complete" {
   source  = "terraform-aws-modules/autoscaling/aws//examples/complete"
   version = "7.6.1"
 
-  name = "blog"
-  min_size = 1
-  max_size = 2 
+  name = "blog-alb"
+  default_autoscaling_group_min_size = 1
+  default_autoscaling_group_max_size = 2 
 
-  vpc_zone_identifier = module.blog_vpc.public_subnets
-  target_group_arns = module.blog_alb.target_group_arns
-  security_groups = [module.blog_sg.security_group_id]
+  default_autoscaling_group_vpc_zone_identifier = module.blog_vpc.public_subnets
+  default_autoscaling_group_target_group_arns = module.blog_alb.target_group_arns
+  vpc_id                                      = module.blog_vpc.vpc_id
 
 }
 
